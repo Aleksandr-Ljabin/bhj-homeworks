664b52f212e6bb75836d40ec17acdd7a98b79d04
Aleksandr-Ljabin <frontdevell@gmail.com>
2021-12-01 11:20:59 +0300

    Исправление задачи №2, 3

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -2,9 +2,15 @@
 const menuLink = document.getElementsByClassName('menu__link');
 for(let i = 0; i < menuLink.length; i++){
 	menuLink[i].onclick = function (){
-		menuLink[i].parentElement.querySelector("ul").className = "menu menu_sub menu_active";
-		return false;
+		if(menuLink[i].parentElement.querySelector("ul") != null){
+			if(menuLink[i].parentElement.querySelector("ul").className == "menu menu_sub"){
+				menuLink[i].parentElement.querySelector("ul").className = "menu menu_sub menu_active";
+			} else if(menuLink[i].parentElement.querySelector("ul").className == "menu menu_sub menu_active"){
+				menuLink[i].parentElement.querySelector("ul").className = "menu menu_sub";
+			  } 
+			return false;
+		}
 	}
-}
+};
 	 
 
--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -1,6 +1,50 @@
 "use strict"
 let sliderItem = Array.from(document.getElementsByClassName('slider__item'));
 let sliderDot = Array.from(document.getElementsByClassName('slider__dot'));
+function slideActive(item){
+	sliderItem[item].className = "slider__item slider__item_active";
+	sliderDot[item].className = "slider__dot slider__dot_active";
+};
+
+let prevSlide = document.getElementsByClassName('slider__arrow slider__arrow_prev');
+prevSlide[0].onclick = function(){
+	let ind = sliderItem.findIndex(item => item.className == "slider__item slider__item_active");
+	sliderItem[ind].className = "slider__item";
+	sliderDot[ind].className = "slider__dot";
+	if(ind == 0){
+		ind = sliderItem.length - 1;
+	} else{
+		ind--
+	}
+	slideActive(ind);
+};
+
+let nextSlide = document.getElementsByClassName('slider__arrow slider__arrow_next');
+nextSlide[0].onclick = function(){
+	let ind = sliderItem.findIndex(item => item.className == "slider__item slider__item_active");
+	sliderItem[ind].className = "slider__item";
+	sliderDot[ind].className = "slider__dot";
+	if(ind == sliderItem.length - 1){
+		ind = 0;
+	} else{
+		ind++;
+	}
+	slideActive(ind);
+};
+
+for (let i = 0; i < sliderDot.length; i++){
+	sliderDot[i].onclick = function() {
+		let ind = sliderItem.findIndex(item => item.className == "slider__item slider__item_active");
+		sliderItem[ind].className = "slider__item";
+		sliderDot[ind].className = "slider__dot";
+		sliderDot[i].className = "slider__dot slider__dot_active";
+		sliderItem[i].className = "slider__item slider__item_active";
+		
+	}
+}
+
+/*let sliderItem = Array.from(document.getElementsByClassName('slider__item'));
+let sliderDot = Array.from(document.getElementsByClassName('slider__dot'));
 let ind = sliderItem.findIndex(item => item.className == "slider__item slider__item_active");
 sliderDot[ind].className = "slider__dot slider__dot_active";
 let prevSlide = document.getElementsByClassName('slider__arrow slider__arrow_prev');
@@ -38,4 +82,6 @@ for (let i = 0; i < sliderDot.length; i++){
 		sliderItem[i].className = "slider__item slider__item_active";
 		
 	}
+	
 }
+ind = sliderItem[ind].findIndex(item => item.className = "slider__dot slider__dot_active")*/
