64f402ad080b3fa7ea9a5eacdac819c136ef0e26
Aleksandr-Ljabin <frontdevell@gmail.com>
2021-12-03 19:02:35 +0300

    Исправление ДЗ

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -2,11 +2,12 @@
 const menuLink = document.getElementsByClassName('menu__link');
 for(let i = 0; i < menuLink.length; i++){
 	menuLink[i].onclick = function (){
-		if(menuLink[i].parentElement.querySelector("ul") != null){
-			if(menuLink[i].parentElement.querySelector("ul").className == "menu menu_sub"){
-				menuLink[i].parentElement.querySelector("ul").className = "menu menu_sub menu_active";
-			} else if(menuLink[i].parentElement.querySelector("ul").className == "menu menu_sub menu_active"){
-				menuLink[i].parentElement.querySelector("ul").className = "menu menu_sub";
+		let parElem = menuLink[i].parentElement.querySelector("ul");
+		if(parElem != null){
+			if(parElem.className == "menu menu_sub"){
+				parElem.className = "menu menu_sub menu_active";
+			} else if(parElem.className == "menu menu_sub menu_active"){
+				parElem.className = "menu menu_sub";
 			  } 
 			return false;
 		}
--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -2,11 +2,13 @@
 let elementsModal = document.getElementsByClassName('modal');
 let close = document.getElementsByClassName("modal__close modal__close_times")
 elementsModal[0].className = "modal modal_active";
-const modalClose = function () {
-	console.log(this.parentElement)
+
+for(let i = 0; i < close.length; i++){
+	close[i].onclick = function(){
+		elementsModal[i].className = "modal";
+	}
 }
 
-close.onclick = modalClose;
 
 const modalShowOpen = function(){
 	if(elementsModal[0].className == "modal modal_active"){
