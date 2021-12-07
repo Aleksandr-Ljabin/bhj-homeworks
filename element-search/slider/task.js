f70a882c834374bb1a8f0e642883b5a4904a5334
Aleksandr-Ljabin <frontdevell@gmail.com>
2021-12-07 11:08:21 +0300

    Исправление задания Слайдер

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -1,11 +1,11 @@
 "use strict"
 let sliderItem = Array.from(document.getElementsByClassName('slider__item'));
 let sliderDot = Array.from(document.getElementsByClassName('slider__dot'));
-function slideInActive(item){
-	sliderItem[item].className = "slider__item";
-	sliderDot[item].className = "slider__dot";
-}
+
 function slideActive(item){
+	let indActive = sliderItem.findIndex(item => item.className == "slider__item slider__item_active");
+	sliderItem[indActive].className = "slider__item";
+	sliderDot[indActive].className = "slider__dot";
 	sliderItem[item].className = "slider__item slider__item_active";
 	sliderDot[item].className = "slider__dot slider__dot_active";
 };
@@ -13,7 +13,6 @@ function slideActive(item){
 let prevSlide = document.getElementsByClassName('slider__arrow slider__arrow_prev');
 prevSlide[0].onclick = function(){
 	let ind = sliderItem.findIndex(item => item.className == "slider__item slider__item_active");
-	slideInActive(ind);
 	if(ind == 0){
 		ind = sliderItem.length - 1;
 	} else{
@@ -25,7 +24,6 @@ prevSlide[0].onclick = function(){
 let nextSlide = document.getElementsByClassName('slider__arrow slider__arrow_next');
 nextSlide[0].onclick = function(){
 	let ind = sliderItem.findIndex(item => item.className == "slider__item slider__item_active");
-	slideInActive(ind);
 	if(ind == sliderItem.length - 1){
 		ind = 0;
 	} else{
@@ -36,10 +34,7 @@ nextSlide[0].onclick = function(){
 
 for (let i = 0; i < sliderDot.length; i++){
 	sliderDot[i].onclick = function() {
-		let ind = sliderItem.findIndex(item => item.className == "slider__item slider__item_active");
-		slideInActive(ind);
-		sliderDot[i].className = "slider__dot slider__dot_active";
-		sliderItem[i].className = "slider__item slider__item_active";
+		slideActive(i)
 		
 	}
 }
